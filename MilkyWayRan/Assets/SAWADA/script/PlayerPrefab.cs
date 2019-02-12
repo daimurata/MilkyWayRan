8a396ae33bdeed26e2f7ac8a19bf4afbe42bd30e
DESKTOP-28F38VP\user <nkc.syougo.sawada@cmail.com>
2019-02-12 11:23:14 +0900

    プレイヤー生成

--- MilkyWayRan/Assets/SAWADA/script/PlayerPrefab.cs
+++ MilkyWayRan/Assets/SAWADA/script/PlayerPrefab.cs
@@ -4,21 +4,40 @@ using UnityEngine;
 
 public class PlayerPrefab : MonoBehaviour
 {
+  //生成するオブジェクト
     public GameObject obj;
+    private GameObject objs;
+    //キーを押す回数
+    int keykount = 0;
     // Start is called before the first frame update
     void Start()
     {
-      
+        //オブジェクト設定
+        objs = obj;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Aキーが押された時
         if (Input.GetKeyDown(KeyCode.A))
         {
-             obj = (GameObject)Resources.Load("player");
-            Instantiate(obj, new Vector3(-13.7f, 1.0f, -4.6f), Quaternion.identity);
-        }
+           //カウントを1個進める
+            keykount += 1;
+            //カウントが1なら
+            if (keykount == 1)
+              objs= Instantiate(obj, new Vector3(-13.7f, 1.0f, -4.6f), Quaternion.identity)as GameObject;//オブジェクト複製
+            //カウントが2なら
+            if (keykount == 2)
+                Destroy(objs);  //オブジェクトを消す
+            //カウントが2以上なら
+            if (keykount >= 2)
+                keykount = 0;//カウントを0にする
+            
+
+                    }
+            }
+      
 
     }
-}
+
