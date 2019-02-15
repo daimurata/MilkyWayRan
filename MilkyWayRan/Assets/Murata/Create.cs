5aa3a0bdda17b5fa2edbb3d00ebc97d959296d76
daimurata <nkc.dai.murata@gmail.com>
2019-02-15 09:48:55 +0900

    タイトルのモーション完成！

---
+++ MilkyWayRan/Assets/Murata/Create.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Create : MonoBehaviour
+{
+    public GameObject Obj;
+    public int CreateObj = 10;
+  
+    void Start()
+    {
+        CreateObject();
+    }
+
+   
+    void Update()
+    {
+        
+    }
+    public void CreateObject()
+    {
+        for(int X = 0; X < CreateObj; X++)
+        {
+            for(int Z = 0; Z < CreateObj; Z++)
+            {
+                Instantiate(Obj, new Vector3(+X, 0, -Z), transform.rotation);
+            }
+        }
+    }
+}
